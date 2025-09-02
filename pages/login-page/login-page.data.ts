96e9d252275baa3b1cadad6ea55213560177d83f
Renato Aguilera <renato.aguilera@valuesite.cl>
2025-09-02 12:08:16 -0400

    definiendo data reutilisable login

---
+++ pages/login-page/login-page.data.ts
@@ -0,0 +1,18 @@
+export class LoginPageData{
+    static get credenciales(){
+        return{
+            user: {
+                standardUser:"standard_user",
+                lockedOutUser:"locked_out_user",
+                problemUser:"problem_user",
+                performanceGlitchUser:"performance_glitch_user",
+            },
+        
+            passw: "secret_sauce",
+            //{
+            //     secretSauce:"secret_sauce",
+            // },
+        }
+
+    }
+}
--- tests/testLogin.spec.ts
+++ tests/testLogin.spec.ts
@@ -1,11 +1,14 @@
 import test, { expect } from "@playwright/test";
 import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { LoginPageMethods } from "../pages/login-page/login-page.methods";
+import { LoginPageData } from "../pages/login-page/login-page.data";
+const usuariosCredenciales = LoginPageData.credenciales;
+
 test('Login', async({page})=>{
     const commonPageMethods = new CommonPageMethods(page)
     const loginPageMethods = new LoginPageMethods(page)
     await commonPageMethods.navegarPrincipal();
-    await loginPageMethods.insertarcredenciales('standard_user','secret_sauce')
+    await loginPageMethods.insertarcredenciales(usuariosCredenciales.user.standardUser,usuariosCredenciales.passw)
     await loginPageMethods.iniciarSesion()
     const elreal = await page.locator('.product_label');
     await expect(elreal).toBeVisible();
